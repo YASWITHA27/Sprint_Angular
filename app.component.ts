4c6cbe3537893f354e584f9129edd4eba90d617e
Yaswitha Sai Atluri <73469122+YASWITHA27@users.noreply.github.com>
2022-04-08 03:23:39 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sprint';
+  constructor(private router:Router) {}
+  OnClick()
+  {
+    this.router.navigateByUrl('home')
+  }
+  OnPress()
+  {
+    this.router.navigateByUrl('login')
+  }
+}
